9a21c5f6b681fbc56cd62e8ffd92d232079c838d
Simon Meggle <simon.meggle@tribe29.com>
2024-11-20 20:33:00 +0100

    fix: RCC entrypoint: build every environment with ROBOCORP_HOME read from .env. After that, export the environment into ZIP files, and import them into the shared holotree. Only in this way the holotree paths are user-definable.

--- scripts/entrypoint-rcc.sh
+++ scripts/entrypoint-rcc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# "internal" ZIPs are the artifacts from hololib builds in /robots.
+# (whereas /hololib_zip are the imported ZIPs)
+HOLOLIB_ZIP_PATH_INT=/hololib_zip_internal
+HOLOLIB_ZIP_PATH=/hololib_zip
+ROBOTS_PATH=/robots
+
 if [ -z "$1" ]; then
     echo "Please provide the mode as an argument: rcc or rccremote."
     exit 1
@@ -57,34 +63,94 @@ import_and_switch_rcc_profile() {
 
 start_rccremote() {
     echo "=== MODE: rccremote"
+    disable_rcc_telemetry
+    build_hololibs
     rcc_init
-    create_and_serve_hl
+    import_hololib_zip_internal
+    import_hololib_zip
+    serve_hololibs
 
 }
 
 rcc_init() {
-    disable_rcc_telemetry
     echo "Enabling shared holotree..."
     rcc ht shared -e && rcc ht init
 }
 
-create_rcc_hololibs() {
-    echo "--- Creating the hololibs..."
-    find /robots -type f -name "robot.yaml" | while read -r robot_yaml; do
-        dir=$(dirname "$robot_yaml")
+build_hololibs() {
+    mkdir -p $HOLOLIB_ZIP_PATH_INT
+    echo "###### Creating the hololibs: #####################################"
+    find $ROBOTS_PATH -type f -name "robot.yaml" | while read -r robot_yaml; do
+        robot=$(dirname "$robot_yaml")
+        echo "Robot: $robot ------------------------------------------"
         # Check if conda.yaml exists in the same directory
-        if [ -f "$dir/conda.yaml" ]; then
-            echo "  + Creating hololib for robot: $dir"
-            rcc ht vars --timeline -r "$robot_yaml"
+        if [ -f "$robot/conda.yaml" ]; then
+            echo "**** ROBOCORP_HOME: $ROBOCORP_HOME"
+            if [ -f "$robot/.env" ]; then
+                echo "  + Found .env file; sourcing it..."
+                cat "$robot/.env"
+                # Save the current environment variables to restore them later
+                saved_env=$(mktemp)
+                export -p >"$saved_env"
+                set -a # (Export all variables sourced from the file)
+                . "$robot/.env"
+                set +a
+            else
+                echo "  - No .env file found; using default env."
+            fi
+            echo "  + Creating hololib for robot: $robot"
+            echo "    BEGIN RCC LOG --- 8< ---"
+            rcc ht vars -r "$robot_yaml"
+            #rcc ht vars --timeline -r "$robot_yaml"
+            echo "    END RCC LOG --- 8< ---"
+
+            # export the hololib to a ZIP
+            echo "  + Exporting hololib to ZIP ($HOLOLIB_ZIP_PATH_INT/$robot.zip)..."
+            rcc ht export -r "$robot_yaml" -z "$HOLOLIB_ZIP_PATH_INT/$robot.zip"
+
+            # if saved env exists, restore it
+            if [ -f "$saved_env" ]; then
+                unset ROBOCORP_HOME
+                . "$saved_env" && rm "$saved_env"
+            fi
         else
-            echo "  - Skipping $dir: conda.yaml not found."
+            echo "  - Skipping $robot: conda.yaml not found."
         fi
+
     done
-    echo "--- Hololib creation finished."
 }
 
-create_and_serve_hl() {
-    create_rcc_hololibs
+import_hololib_zip_internal() {
+    echo "Importing hololib ZIP artifacts from $HOLOLIB_ZIP_PATH_INT to shared holotree..."
+    find $ROBOTS_PATH -type f -name "robot.yaml" | while read -r robot_yaml; do
+        robot=$(dirname "$robot_yaml")
+        if [ -f "$HOLOLIB_ZIP_PATH_INT/$robot.zip" ]; then
+            echo "> importing Robot $robot from $HOLOLIB_ZIP_PATH_INT/$robot.zip..."
+            rcc holotree import "$HOLOLIB_ZIP_PATH_INT/$robot.zip"
+        else
+            echo "WARNING! $HOLOLIB_ZIP_PATH_INT/$robot.zip not found."
+        fi
+    done
+    print_catalogs
+}
+
+import_hololib_zip() {
+    echo "Importing mounted hololib ZIP artifacts from $HOLOLIB_ZIP_PATH to shared holotree..."
+    for zip in $HOLOLIB_ZIP_PATH/*.zip; do
+        echo "> importing $zip ..."
+        rcc holotree import "$zip"
+    done
+    print_catalogs
+}
+
+print_catalogs() {
+    echo "Holotree catalogs:"
+    echo "ROBOCORP_HOME: $ROBOCORP_HOME"
+    rcc ht catalogs
+    echo ""
+}
+
+serve_hololibs() {
     echo "Starting rccremote..."
     rccremote -hostname 0.0.0.0 -debug -trace
     # TODO: does not work
@@ -93,18 +159,18 @@ create_and_serve_hl() {
     #echo "rccremote started with PID: $RCCREMOTE_PID"
 }
 
-monitor_and_restart() {
-    echo "--- Monitoring '/robots' for changes..."
-    while true; do
-        inotifywait -r -e modify,create,delete /robots
-        echo "!   Change detected in /robots. Restarting rccremote..."
-        if [ -n "$RCCREMOTE_PID" ]; then
-            kill "$RCCREMOTE_PID"
-            wait "$RCCREMOTE_PID" 2>/dev/null
-            echo "Stopped rccremote (PID: $RCCREMOTE_PID)"
-        fi
-        create_and_serve_hl
-    done
-}
+# monitor_and_restart() {
+#     echo "--- Monitoring '$ROBOTS_PATH' for changes..."
+#     while true; do
+#         inotifywait -r -e modify,create,delete $ROBOTS_PATH
+#         echo "!   Change detected in $ROBOTS_PATH. Restarting rccremote..."
+#         if [ -n "$RCCREMOTE_PID" ]; then
+#             kill "$RCCREMOTE_PID"
+#             wait "$RCCREMOTE_PID" 2>/dev/null
+#             echo "Stopped rccremote (PID: $RCCREMOTE_PID)"
+#         fi
+#         build_hololibs
+#     done
+# }
 
 main "$@"
